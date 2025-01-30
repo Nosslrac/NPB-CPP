9ed7bdbc49e1e4b5fa1674cae821a58df6ab1388
Axel Carlsson <axel.00@live.se>
2025-01-30 13:12:36 +0100

    Update build script

--- NPB-OMP/instantiateBuild.sh
+++ NPB-OMP/instantiateBuild.sh
@@ -5,9 +5,31 @@
 # Set environment variable OPENMP_DIR to point to the runtime build directory.
 
 cd ./config
-source ~/.bashrc.user
+
+OMP=''
+if [ -z "$1" ]; then
+    source ~/.bashrc
+    if [ -z "$OPENMP_DIR" ]; then 
+        echo "OpenMP directory environment variable not set:"
+        echo "Set OPENMP_DIR=<path to OpenMP runtime lib> in your ~/.bashrc.user"
+        exit 1
+    fi
+    OMP=$OPENMP_DIR
+else
+    OMP=$1
+fi
+
+if [ -z "$(grep -E "C_INC = .+? -I$OMP" ./make.def)" ]; then
+    echo "OMP include not found adding include"
+    sed -i "s|C_INC = .*$|& -I$OMP|" make.def
+fi
+
+if [ -z "$(grep -E "CLINKFLAGS =.+?-L$OMP -lomp" ./make.def)" ]; then
+    echo "OMP include not found adding include"
+    sed -i "s|CLINKFLAGS =.*$|& -L$OMP -lomp|" make.def
+fi
+exit 0
 
 
-sed -i "s|C_INC = .*$|&-I$OPENMP_DIR|" make.def
 
-sed -i "s|CLINKFLAGS =.*$|&-L$OPENMP_DIR -lomp|" make.def
+
